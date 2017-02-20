da110661cdbae113521606dfce5e763bc2a6bca0
ameur <ameur.drira@gmail.com>
2017-02-21 00:15:28 +0100

    topic

---
+++ README.md
@@ -0,0 +1,2 @@
+This is an MQTT Consumer listening for LoRa frames coming from soitech.cloudapp.net IoT platform.
+Please adapt the topic “NewAirQuality” to your LoRa application name or your device address.
--- src/soitech/MqttLoRaClient.java
+++ src/soitech/MqttLoRaClient.java
@@ -5,23 +5,23 @@ import java.util.Calendar;
 
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
-public class UplinkMqttClient extends DefaultMqttClient {
+public class MqttLoRaClient extends DefaultMqttClient {
 
-	public UplinkMqttClient(String topic) {
+	public MqttLoRaClient(String topic) {
 		super(topic);
-		
+
 	}
+
 	@Override
 	public void handle(String channel, MqttMessage message) {
 		String timeStamp = new SimpleDateFormat("yyyy/MM/dd  HH:mm:ss").format(Calendar.getInstance().getTime());
-
 		System.out.println(timeStamp + " " + channel + " Message  : " + message);
 
 	}
 
 	public static void main(String[] args) {
-		UplinkMqttClient uplinkMqttClient=new UplinkMqttClient("lora");
-		uplinkMqttClient.start();
+		MqttLoRaClient mqttLoRaClient = new MqttLoRaClient("NewAirQuality");
+		mqttLoRaClient.start();
 
 	}
 
